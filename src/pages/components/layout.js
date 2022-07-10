d1cd5e898bf40cebed1ca4a3745312601be9f676
WsMonty <grethen.gilles@gmail.com>
2022-07-10 11:04:24 +0200

    more SEO

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from '../nav.js';
 import '../../scss/main.scss';
-import { Helmet } from 'react-helmet';
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
 
 const Layout = ({ children }) => {
   return (
     <div className="main">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Gilles Grethen</title>
-      </Helmet>
+      <GatsbySeo
+        title="Gilles Grethen"
+        description={`Gilles Grethen\'s Website about his music and projects.`}
+        language="en"
+        noindex={false}
+        nofollow={false}
+      />
       <Navbar />
       {children}
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,9 @@
 import * as React from 'react';
 import '../scss/main.scss';
-import { Helmet } from 'react-helmet';
 
 // markup
 const IndexPage = () => {
-  return (
-    <div className="main">
-      <Helmet>
-        <meta lang="en" charSet="utf-8"></meta>
-        <title>Gilles Grethen</title>
-        <link rel="canonical" href="gillesgrethen.com" />
-      </Helmet>
-    </div>
-  );
+  return <div className="main"></div>;
 };
 
 export default IndexPage;
