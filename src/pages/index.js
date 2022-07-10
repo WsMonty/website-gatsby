5577424e0129e06109aa38ac269439558569273f
WsMonty <grethen.gilles@gmail.com>
2022-07-10 10:54:19 +0200

    SEO changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import '../scss/main.scss';
+import { Helmet } from 'react-helmet';
 
 // markup
 const IndexPage = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <Helmet>
+        <meta lang="en" charSet="utf-8"></meta>
+        <title>Gilles Grethen</title>
+        <link rel="canonical" href="gillesgrethen.com" />
+      </Helmet>
+    </div>
+  );
 };
 
 export default IndexPage;
