602d710c9a11a1118ea36b141e672949d538f072
WsMonty <grethen.gilles@gmail.com>
2022-10-14 11:50:09 +0200

    need to grab pics

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { useState } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Navbar = (props) => {
   const links = [
@@ -62,9 +63,46 @@ const Navbar = (props) => {
         </button>
       </div>
       <div className="navbar_link_socials_container">
-        <div>Youtube</div>
-        <div>Insta</div>
-        <div>Face</div>
+        <a
+          href="https://www.instagram.com/gilles_grethen_/"
+          target={'_blank'}
+          rel="noreferrer"
+        >
+          <StaticImage
+            className="sidesocials_icon"
+            src="../../imgs/Insta-logo-whiteblue.png"
+            alt="Instagram Logo"
+            placeholder="blurred"
+          />
+        </a>
+      </div>
+      <div className="sidesocials_icon_wrapper">
+        <a
+          href="https://www.youtube.com/channel/UCgZez7ebsQQFIO83RDIj8zQ"
+          target={'_blank'}
+          rel="noreferrer"
+        >
+          <StaticImage
+            className="sidesocials_icon"
+            src="../../imgs/yt-logo-whiteblue.png"
+            alt="Youtube Logo"
+            placeholder="blurred"
+          />
+        </a>
+      </div>
+      <div className="sidesocials_icon_wrapper">
+        <a
+          href="https://www.facebook.com/gillesgrethen"
+          target={'_blank'}
+          rel="noreferrer"
+        >
+          <StaticImage
+            className="sidesocials_icon"
+            src="../../imgs/fb-logo-whiteblue.png"
+            alt="Facebook Logo"
+            placeholder="blurred"
+          />
+        </a>
       </div>
     </div>
   );
